cb774354db001d2a248167c62fed69c51e0ca870
Kwonhong1 <gggghhhh8211@gmail.com>
2026-07-24 21:33:52 +0900

    7/24 changes
    for the resposibility of player's position, func that make moving of player puts in player

--- player.gd
+++ player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 @export var speed:int
 var move_direction:Vector2=Vector2.ZERO
-
+func _ready() -> void:
+	RoomManager.room_changed.connect(_on_room_changed)
 
 func _physics_process(delta: float) -> void:
 	movement_loop()
@@ -15,5 +16,31 @@ func movement_loop() ->void:
 	var motion: Vector2=move_direction.normalized() *speed
 	velocity=motion
 	move_and_slide()
+
+
+# when interact with doors
+func _on_room_changed(active_room: Node2D, direction: String) -> void:
+	var door_node_path = ""
+	var offset_vec = Vector2.ZERO
+	var door_offset = 130.0 # 문에 다시 닿지 않도록 여유 공간 부여
 	
+	# 들어온 방향의 반대쪽 문 앞으로 배치
+	match direction:
+		"up":
+			door_node_path = "Doors/DoorDown"
+			offset_vec = Vector2(0, -door_offset)
+		"down":
+			door_node_path = "Doors/DoorUp"
+			offset_vec = Vector2(0, door_offset)
+		"left":
+			door_node_path = "Doors/DoorRight"
+			offset_vec = Vector2(-door_offset, 0)
+		"right":
+			door_node_path = "Doors/DoorLeft"
+			offset_vec = Vector2(door_offset, 0)
+			
+	var target_door = active_room.get_node_or_null(door_node_path)
+	if target_door:
+		global_position = target_door.global_position + offset_vec
+		velocity = Vector2.ZERO
 	
